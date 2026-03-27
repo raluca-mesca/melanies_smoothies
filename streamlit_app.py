8a0741300ace3598b50a3b80c00c60f5a9c0db9f
raluca-mesca <47624841+raluca-mesca@users.noreply.github.com>
2026-03-27 17:26:28 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages.
 import streamlit as st
+import pandas as pd
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 import requests
@@ -27,6 +28,11 @@ print('HERE', session)
 #my_dataframe = session.table("fruit_options") ...
 my_dataframe = session.table("fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
+
+# Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 
 # adjusting below!
