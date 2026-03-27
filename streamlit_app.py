f4bd8769bc7815f1665aff16d2e74332e539f80b
raluca-mesca <47624841+raluca-mesca@users.noreply.github.com>
2026-03-27 16:38:02 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,14 +72,14 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
 
-#import requests  
-#smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-#st.text(smoothiefroot_response)
-
 import requests  
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+st.text(smoothiefroot_response.json())
 
-smoothiefroot_response = requests.get(
-    "https://my.smoothiefroot.com/api/fruit/watermelon"
-)
+#import requests  
 
-st.json(smoothiefroot_response.json())
+#smoothiefroot_response = requests.get(
+#    "https://my.smoothiefroot.com/api/fruit/watermelon"
+#)
+
+#st.json(smoothiefroot_response.json())
