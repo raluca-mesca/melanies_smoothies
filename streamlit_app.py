e1db9ba5386b13a55d7b8370bc96d8b28af7a01d
raluca-mesca <47624841+raluca-mesca@users.noreply.github.com>
2026-03-27 17:14:17 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,9 @@ session = cnx.session()
 print('HERE', session)
 my_dataframe = session.table("fruit_options")
 # my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+# adjusting below!
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 print('my_dataframe')
 print(my_dataframe)
 
