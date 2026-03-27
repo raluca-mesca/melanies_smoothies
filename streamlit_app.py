9ef28bee320f5d87bc8cd1add1755c1b6b6292e9
raluca-mesca <47624841+raluca-mesca@users.noreply.github.com>
2026-03-27 17:31:26 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,8 +26,8 @@ session = cnx.session()
 print('HERE', session)
 my_dataframe = session.table("fruit_options")
 #my_dataframe = session.table("fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop()
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
 
 # Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
 #pd_df=my_dataframe.to_pandas()
